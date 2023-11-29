ca927ae9002dff2eac1c7c21b073cf57aad32df8
Alexander <aalexanderfc@gmail.com>
2023-11-30 00:17:25 +0100

    midnight

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 #include <unistd.h>
 #include <time.h>
 
-typedef struct {
+typedef struct {//struct för kort
     char cardID[20];
     int access;
-    char date[20];
+    char registrationDate[20];
 } CARD;
 
-typedef struct {
+typedef struct {//struct för lista av kort
     CARD *lista;
     int count;
 } CARDLIST;
 
-const char *currentDate() {
+const char *currentDate() {//funktion som returnerar dagens datum
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     char *date = (char *)malloc(sizeof(char) * 20);
@@ -32,10 +32,13 @@ void addCardToList(CARDLIST *cardList, char *cardID, int access, const char *dat
 
     strcpy(cardList->lista[count].cardID, cardID);
     cardList->lista[count].access = access;
+
     if(updateDate){//if updateDate is true
-        strcpy(cardList->lista[count].date, currentDate());
+    const char *currentDateStr = currentDate();
+        strcpy(cardList->lista[count].registrationDate, currentDateStr);
+        free((void *)currentDateStr);
     }else{
-    strcpy(cardList->lista[count].date, date);
+        strcpy(cardList->lista[count].registrationDate, date);
     }
     cardList->count++;
 }
@@ -45,19 +48,8 @@ void fakeTestScanCard(){
     char cardNumber[20];
     GetInput("Enter card number: ", cardNumber, 10);
     printf("cardID: %s\n", cardNumber);
-    
-    
-}
-//funktion som skriver ut menyn för admin:
-void adminAccessMenu() {
-    printf("CURRENTLY LAMP IS: Orange\n");
-    printf("\n");
-    printf("1. Remote open door\n");
-    printf("2. List logged in\n");
-    printf("3. Add/remove access\n");
-    printf("4. Back\n");
-    printf("\n");
-}
+    }
+
 //funktion som returnerar indexet för ett kort i listan:
 int getCardIndex(CARD *lista, int count, char *cardID) {
     for (int i = 0; i < count; i++) {
@@ -67,6 +59,106 @@ int getCardIndex(CARD *lista, int count, char *cardID) {
     }
     return -1;
 }
+//funktion som skriver ut menyn för admin:
+// return 1 if success, 0 if wrong entry
+int adminAccessMenu(CARDLIST *cardList) {
+    printf("CURRENTLY LAMP IS: Orange\n");
+    printf("\n");
+    printf("1. Add/remove access\n");
+    printf("2. Back\n");
+    printf("\n");
+
+    int menuSel3;
+    GetInputInt("Ange val: ", &menuSel3);
+
+    if (menuSel3 == 1) {
+        char cardNumber[20];
+        GetInput("Enter card number: ", cardNumber, 10);
+        printf("cardID: %s\n", cardNumber);
+        int index = getCardIndex(cardList->lista, cardList->count, cardNumber);
+        printf("index: %d\n", index);
+        if (index == -1) {
+            printf("Card not found!\n");
+
+            char answer[4];
+            while (1) {
+                GetInput("Do you want to add this card to the system?:(yes/no)", answer, 4);
+                if (strcmp(answer, "yes") == 0) {
+                    int accessAnswer;
+                    printf("The recently added card has no access by default!\n");
+                    sleep(2);
+                    printf("Do you want to give access to this card?\n");
+                    GetInputInt("1. for access, 2. for no access, 3. Go back: ", &accessAnswer);
+
+                    if (accessAnswer == 1) {
+                        addCardToList(cardList, cardNumber, 1, currentDate(), 1);
+                        printf("Access granted!\n");
+                        printf("\n");
+                    } else if (accessAnswer == 2) {
+                        addCardToList(cardList, cardNumber, 0, currentDate(), 1);
+                        printf("Access removed!\n");
+                        printf("\n");
+                    } else if (accessAnswer == 3) {
+                        break;
+                    } else {
+                        printf("Wrong input!\n");
+                    }
+
+                    printf("Card added!\n");
+                    printf("\n");
+                    break;
+
+                } else if (strcmp(answer, "no") == 0) {
+                    printf("Card not added!\n");
+                    printf("\n");
+                    break;
+                } else {
+                    printf("Wrong input!\n");
+                }
+
+                GetInputChar("Press enter to continue", NULL);
+            }
+        } else {
+            if (cardList->lista[index].access == 0) {
+                printf("This card has no access!\n");
+                int answer;
+                GetInputInt("1. for access, 2. for no access: ", &answer);
+                if (answer == 1) {
+                    cardList->lista[index].access = 1;
+                    printf("Access granted!\n");
+                    printf("\n");
+                } else if (answer == 2) {
+                    cardList->lista[index].access = 0;
+                    printf("Access removed!\n");
+                    printf("\n");
+                } else {
+                    printf("Wrong input!\n");
+                }
+            } else {
+                printf("This card has access!\n");
+                int answer;
+                GetInputInt("1. for access, 2. for no access: ", &answer);
+                if (answer == 1) {
+                    cardList->lista[index].access = 1;
+                    printf("Access granted!\n");
+                    printf("\n");
+                } else if (answer == 2) {
+                    cardList->lista[index].access = 0;
+                    printf("Access removed!\n");
+                    printf("\n");
+                } else {
+                    printf("Wrong input!\n");
+                }
+            }
+        }
+    } else if (menuSel3 == 2) {
+        // Do nothing, go back
+    } else {
+        printf("Wrong input!\n");
+    }
+    return 1;
+}
+
 //funktion som skriver ut menyn för admin och hanterar input:
 void adminMenu(CARDLIST *cardList) {
     while (1) {
@@ -78,6 +170,8 @@ void adminMenu(CARDLIST *cardList) {
         printf("4. Exit\n");
         printf("9. FAKE TEST SCAN CARD\n");
         printf("\n");
+
+
 
         int sel;
 
@@ -100,100 +194,25 @@ void adminMenu(CARDLIST *cardList) {
                     strcpy(accessOrNot, "Access");
                 }
 
-                printf("%s %s Added to system %s \n", cardList->lista[i].cardID, accessOrNot, cardList->lista[i].date);
+                printf("%s %s Added to system %s \n", cardList->lista[i].cardID, accessOrNot, cardList->lista[i].registrationDate);
             }
             GetInputChar("Press enter to continue", NULL);
 
-        } else if (sel == 3) {//add/remove access
-            adminAccessMenu();
-            char cardNumber[20];
-            GetInput("Enter card number: ", cardNumber, 10);
-            printf("cardID: %s\n", cardNumber);
-            int index = getCardIndex(cardList->lista, cardList->count, cardNumber);
-            printf("index: %d\n", index);
-            if (index == -1) {
-                printf("Card not found!\n");
-                char answer[4];
-                while (1) {
-                    GetInput("Do you want to add this card to the system?:(yes/no)", answer, 4);
-                    if (strcmp(answer, "yes") == 0) {
-                        int accessAnswer;
-                        printf("The recently added card has no access by default!\n");
-                        sleep(2);
-                        printf("Do you want to give access to this card?\n");
-                        GetInputInt("1. for access, 2. for no access: ", &accessAnswer);
-
-                        if(accessAnswer == 1){
-                            addCardToList(cardList, cardNumber, 1, currentDate(), 1);
-                            printf("Access granted!\n");
-                            printf("\n");
-                        }else if(accessAnswer == 2){
-                            addCardToList(cardList, cardNumber, 0, currentDate(), 1);
-                            printf("Access removed!\n");
-                            printf("\n");
-                        }else{
-                            printf("Wrong input!\n");
-                        }
-
-                        printf("Card added!\n");
-                        printf("\n");
-                        break;
-
-                    } else if (strcmp(answer, "no") == 0) {
-                        printf("Card not added!\n");
-                        printf("\n");
-                        break;
-                    } else {
-                        printf("Wrong input!\n");
-                    }
-                }
-                GetInputChar("Press enter to continue", NULL);
-                continue;
-
-            } else {
-                if (cardList->lista[index].access == 0) {
-                    printf("This card has no access!\n");
-                    int answer;
-                    GetInputInt("1. for access, 2. for no access: ", &answer);
-                    if (answer == 1) {
-                        cardList->lista[index].access = 1;
-                        printf("Access granted!\n");
-                        printf("\n");
-                    } else if (answer == 2) {
-                        cardList->lista[index].access = 0;
-                        printf("Access removed!\n");
-                        printf("\n");
-                    } else {
-                        printf("Wrong input!\n");
-                    }
-                } else {
-                    printf("This card has access!\n");
-                    int answer;
-                    GetInputInt("1. for access, 2. for no access: ", &answer);
-                    if (answer == 1) {
-                        cardList->lista[index].access = 1;
-                        printf("Access granted!\n");
-                        printf("\n");
-                    } else if (answer == 2) {
-                        cardList->lista[index].access = 0;
-                        printf("Access removed!\n");
-                        printf("\n");
-                    } else {
-                        printf("Wrong input!\n");
-                    }
-                }
-            }
-
-        } else if (sel == 4) {//exit
+        } else if (sel == 3) 
+        {//add/remove access
+            adminAccessMenu(cardList);
+            
+        }else if (sel == 4) {//exit
             break;
         }else if(sel == 9){
-
-        } 
-        else {
+            printf("Fake card reader");
+        }else{
             printf("Wrong input. Try again!\n");
         }
+        }
+        
     }
-}
+
 
 int main() {
     printf("Current date: %s\n", currentDate());
@@ -202,8 +221,8 @@ int main() {
     cardList.count = 0;
 
     
-    addCardToList(&cardList, "23", 0, "2023-11-24", 1);
-    addCardToList(&cardList, "34", 1, "2023-11-24", 1);
+    addCardToList(&cardList, "1023", 0, "2023-11-24", 1);
+    addCardToList(&cardList, "1024", 1, "2023-11-24", 1);
 
     adminMenu(&cardList);
 
